d758ec982e408119eb7e86a8e663f3d852521e01
Seiji Shaw <seiji.shaw@gmail.com>
2024-01-27 15:15:50 -0500

    record results from one round of experiments in the dataframe.

--- nonasymptotic/exps.py
+++ nonasymptotic/exps.py
@@ -9,17 +9,23 @@ def straight_line_trial(delta_clear, epsilon_tol, dim, rng_seed,
                         sample_schedule=None,
                         n_samples_per_ed_check_round=100,
                         ed_check_timeout=60.0,
-                        area_tol=1e-8):
+                        area_tol=1e-8,
+                        max_k_connection_neighbors=2048):
     if sample_schedule is None:
-        sample_schedule = [10, 100, 1000, 10000]
+        # default 'small experiment' sample schedule
+        sample_schedule = [10, 100, 1000]
 
     # set up experiment objs
     conn_radius = 2 / np.sqrt(1 + epsilon_tol ** 2) * (epsilon_tol + delta_clear)
     env = StraightLine(dim=dim, delta_clearance=delta_clear, seed=rng_seed)
-    prm = SimplePRM(conn_radius, env.is_motion_valid, env.sample_from_env, env.distance_to_path, seed=rng_seed)
+    prm = SimplePRM(conn_radius, env.is_motion_valid, env.sample_from_env, env.distance_to_path,
+                    max_k_connection_neighbors=max_k_connection_neighbors, seed=rng_seed)
 
-    # run along sample schedule. Once we are ed-complete, we always will be, so we skip the rest when we are.
-    # TODO: add record keeping mechanism to return
+    # lists to save records (we'll form a dataframe afterward)
+    ed_complete_record = []
+    info_record = []
+
+    # run along sample schedule. Once we are ed-complete, we always will be, so we skip the rest when we are.\
     is_ed_complete = False
     for n_samples in sample_schedule:
 
@@ -32,5 +38,21 @@ def straight_line_trial(delta_clear, epsilon_tol, dim, rng_seed,
                                                                      n_samples_per_check=n_samples_per_ed_check_round,
                                                                      timeout=ed_check_timeout,
                                                                      area_tol=area_tol)
+
+            ed_complete_record.append(is_ed_complete)
+            info_record.append(info)
+
         else:
-            pass
+            ed_complete_record.append(True)
+            info_record.append('covered with fewer samples')
+
+    # construct dataframe and return
+    df_record = pd.DataFrame(
+        {
+            'n_samples': sample_schedule,
+            'ed_complete': ed_complete_record,
+            'info': info_record,
+        }
+    )
+
+    return df_record
