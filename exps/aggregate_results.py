366992058eed0831c0cb46fd353b10d4b72dbdae
Seiji Shaw <seiji.shaw@gmail.com>
2024-01-28 23:00:35 -0500

    fix directories for agg out

--- exps/aggregate_results.py
+++ exps/aggregate_results.py
@@ -5,6 +5,7 @@ some statistics about the _run_. Figures will be computed in notebooks.
 import pandas as pd
 import argparse
 import glob
+import os
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -12,7 +13,11 @@ if __name__ == '__main__':
     parser.add_argument('--df-out-name', type=str, default=None)
     args = parser.parse_args()
 
-    mini_out_csvs = glob.glob('results/%s*/out*.csv' % args.name_prefix)
+    exp_dirs = glob.glob('results/%s*' % args.name_prefix)
+    exp_dirs.sort(key=os.path.getmtime)
+    latest_exp_dir = exp_dirs[-1]
+
+    mini_out_csvs = glob.glob(latest_exp_dir + '/out*.csv' % args.name_prefix)
     big_out = pd.concat(list(map(pd.read_csv, mini_out_csvs)))
 
     # time statistics
@@ -22,8 +27,6 @@ if __name__ == '__main__':
     print('Trial exec time mean: %f +/- %f' % (mean, std))
 
     if args.df_out_name is not None:
-        big_out.to_csv('results/' + args.df_out_name + '_agg_out.csv')
+        big_out.to_csv(latest_exp_dir + '/' + args.df_out_name + '_agg_out.csv')
     else:
-        big_out.to_csv('results/' + args.name_prefix + '_agg_out.csv')
-
-
+        big_out.to_csv(latest_exp_dir + '/' + args.name_prefix + '_agg_out.csv')
